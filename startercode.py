39c9f539b7360c63cfc996982e2870bb6012a803
Kings Sugar Daddy <kingssugardaddy@Kingss-MacBook-Air.local>
2026-04-06 22:50:17 -0400

    First message

--- startercode.py
+++ startercode.py
@@ -1,7 +1,7 @@
 # SI 201 HW6 (APIs, JSON, and Caching)
-# Your name:
-# Your student id:
-# Your email:
+# Your name: King
+# Your student id: 23442611
+# Your email: kingjj@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
@@ -36,7 +36,12 @@ def load_json(filename):
         A dictionary with the JSON data, OR an empty dictionary {} if the file
         cannot be opened or is not valid JSON.
     """
-    pass
+    
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
 
 
 def create_cache(dictionary, filename):
